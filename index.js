78077ee5ea85c676ab56bb365a706d0f4da0b94d
“upolk” <luck@Luck-Pro.local>
2024-03-15 19:14:23 +0800

    add repository url

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // useGenerateWallets.js
-const generateWallets = require('zoahn');
+const generateWallets = require('upolka');
 
 // 使用第一个项目的功能
-generateWallets(3, 'matthewmed.xlsx');
+generateWallets(3, 'upo.xlsx');
